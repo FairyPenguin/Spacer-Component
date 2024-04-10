df497016b058e2f39598865b92dcd7b2adbdf134
FairyPenguin <autorun81@gmail.com>
2024-04-11 01:36:48 +0200

    Fix types missing bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Spacer from "./components/Spacer/Spacer";
-
+// import Spacer from "./components/Spacer/Spacer";
+// import { Spacer } from "spacer-component";
+import { ComponentName } from "../node_modules/spacer-component/dist/spacer-component.es.js";
 function App() {
   return (
     <>
       <h1></h1>
-      <Spacer space={"0.25rem"} />
+
+      {/* <Spacer space={"0.25rem"} /> */}
     </>
   );
 }
